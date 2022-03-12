7b0765a2f1330cc90365feeca60567ceb1ccefa3
1003 fengye <mcfengye1003@outlook.com>
2022-03-12 13:42:34 +0800

    Logical-drives scanning added.

--- AutoFileBAK/FileSystemReflector.cs
+++ AutoFileBAK/FileSystemReflector.cs
@@ -1,12 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
 using System.Collections;
-using System.Threading;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace AutoFileBAK
 {
+    static public class DiskIDHelper
+    {
+        [DllImport("kernel32.dll")]
+        private static extern int GetVolumeInformation
+        (
+            string lpRootPathName,
+            string lpVolumeNameBuffer,
+            int nVolumeNameSize,
+            ref int lpVolumeSerialNumber,
+            int lpMaximumComponentLength,
+            int lpFileSystemFlags,
+            string lpFileSystemNameBuffer,
+            int nFileSystemNameSize
+        );
+
+        /// <summary>
+        /// 获得盘符为drvID的硬盘序列号，缺省为C
+        /// </summary>
+        /// <param name="drvID"></param>
+        /// <returns></returns>
+        static public string GetID(string drvID)
+        {
+            const int MAX_FILENAME_LEN = 256;
+            int retVal = 0;
+            int a = 0;
+            int b = 0;
+            string str1 = null;
+            string str2 = null;
+            int i = GetVolumeInformation
+                (
+                drvID,
+                str1,
+                MAX_FILENAME_LEN,
+                ref retVal,
+                a,
+                b,
+                str2,
+                MAX_FILENAME_LEN
+                );
+            return retVal.ToString();
+        }
+        static public string GetID()
+        {
+            const int MAX_FILENAME_LEN = 256;
+            int retVal = 0;
+            int a = 0;
+            int b = 0;
+            string str1 = null;
+            string str2 = null;
+            int i = GetVolumeInformation
+                (
+                "C:\\",
+                str1,
+                MAX_FILENAME_LEN,
+                ref retVal,
+                a,
+                b,
+                str2,
+                MAX_FILENAME_LEN
+                );
+            return retVal.ToString();
+        }
+    }
     internal class FileSystemReflector
     {
         public static void CheckForImage(string Path, string ToPath)
@@ -75,11 +136,9 @@ namespace AutoFileBAK
                 }
                 //Part of files.
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Log.SaveLog("Exception :" + ex.ToString());
-                Thread.Sleep(10000);
-                CheckForImage(Path, ToPath);
             }
         }
     }
--- AutoFileBAK/Program.cs
+++ AutoFileBAK/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
-using System.Threading;
 using System.Linq;
+using System.Threading;
 
 namespace AutoFileBAK
 {
@@ -10,47 +9,57 @@ namespace AutoFileBAK
     {
         static void Main(string[] args)
         {
-            FileSystemReflector.CheckForImage("D:/ps", "D:/result");
-            //string[] Drives;
-            //string[] NowDrives;
-            //switch (args.Length)
-            //{
-            //    case 0:
-            //        ShadowProcess.MainProcess(true);
-            //        break;
-            //    case 1:
-            //        switch (args[0])
-            //        {
-            //            case "--StartListening":
-            //                Drives = Environment.GetLogicalDrives();
-            //                while (true)
-            //                {
-            //                    //string[] arrRate = new string[] { "a", "b", "c", "d" };//A
-            //                    //string[] arrTemp = new string[] { "c", "d", "e" };//B
-            //                    //string[] arrUpd = arrRate.Intersect(arrTemp).ToArray();//相同的数据 （结果：c,d）
-            //                    //string[] arrAdd = arrRate.Except(arrTemp).ToArray();//A中有B中没有的 （结果：a,b）
-            //                    //string[] arrNew = arrTemp.Except(arrRate).ToArray();//B中有A中没有的 （结果：e）
-            //                    NowDrives = Environment.GetLogicalDrives();
-            //                    string[] NewDrives = NowDrives.Except(Drives).ToArray();
-            //                    if (NewDrives.Length == 0)
-            //                    {
-            //                        Thread.Sleep(30000);
-            //                    }
-            //                    else
-            //                    {
-            //                        foreach(string Drive in NewDrives)
-            //                        {
-            //                            Log.SaveLog(Drive);
-            //                        }
-            //                    }
-            //                }
-            //            case "--RunAsShadowProcess":
-            //                Log.SaveLog(Process.GetCurrentProcess().MainModule.FileName + " started.");
-            //                ShadowProcess.RunAsShadowProcess();
-            //                break;
-            //        }
-            //        break;
-            //}
+            //FileSystemReflector.CheckForImage("F:/", "K:/BACKUP/");
+            string[] Drives;
+            string[] NowDrives;
+            switch (args.Length)
+            {
+                case 0:
+                    ShadowProcess.MainProcess(false);
+                    break;
+                case 1:
+                    switch (args[0])
+                    {
+                        case "--StartListening":
+                            Drives = Environment.GetLogicalDrives();
+                            while (true)
+                            {
+                                //string[] arrRate = new string[] { "a", "b", "c", "d" };//A
+                                //string[] arrTemp = new string[] { "c", "d", "e" };//B
+                                //string[] arrUpd = arrRate.Intersect(arrTemp).ToArray();//相同的数据 （结果：c,d）
+                                //string[] arrAdd = arrRate.Except(arrTemp).ToArray();//A中有B中没有的 （结果：a,b）
+                                //string[] arrNew = arrTemp.Except(arrRate).ToArray();//B中有A中没有的 （结果：e）
+                                NowDrives = Environment.GetLogicalDrives();
+                                string[] NewDrives = NowDrives.Except(Drives).ToArray();
+                                string DriveStringList = "";
+                                foreach (string DriveItem in NowDrives)
+                                {
+                                    DriveStringList += DriveItem + ",";
+                                    Drives = NowDrives;
+                                }
+                                if (NewDrives.Length == 0)
+                                {
+                                    Log.SaveLog("No new devices detected:" + DriveStringList);
+                                    Thread.Sleep(10000);
+                                }
+                                else
+                                {
+                                    foreach (string Drive in NewDrives)
+                                    {
+                                        Log.SaveLog(Drive + " will be checked.");
+                                        FileSystemReflector.CheckForImage(Drive, "K:/" + DiskIDHelper.GetID(Drive) + "-Image");
+                                    }
+                                    Drives = NowDrives;
+                                }
+                            }
+                        case "--RunAsShadowProcess":
+                            Log.SaveLog(Process.GetCurrentProcess().MainModule.FileName + " started.");
+                            ShadowProcess.RunAsShadowProcess();
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
+
